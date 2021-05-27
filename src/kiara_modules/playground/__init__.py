236f3c452a99aa6dcb8ed4c50889877855c18244
Markus Binsteiner <markus@frkl.io>
2021-05-27 11:12:57 +0200

    chore: sync project template

--- src/kiara_modules/playground/__init__.py
+++ src/kiara_modules/playground/__init__.py
@@ -6,10 +6,22 @@
 import logging
 import os
 
+from kiara import (
+    KiaraEntryPointItem,
+    find_kiara_modules_under,
+    find_kiara_pipelines_under,
+)
+
 __author__ = """Markus Binsteiner"""
 __email__ = "markus.binsteiner@uni.lu"
 
 log = logging.getLogger("kiara_modules")
+
+modules: KiaraEntryPointItem = (find_kiara_modules_under, ["kiara_modules.playground"])
+pipelines: KiaraEntryPointItem = (
+    find_kiara_pipelines_under,
+    ["kiara_modules.playground"],
+)
 
 
 def get_version():
