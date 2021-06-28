ae96f1647d74c4f27d5fa27309316e8bbd65e8c5
Markus Binsteiner <markus@frkl.io>
2021-06-28 10:37:21 +0200

    refactor: moved lenas code into its own Python module

---
+++ src/kiara_modules/playground/lena.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import typing
+
+import networkx as nx
+from kiara import KiaraModule
+from kiara.data.values import ValueSchema, ValueSet
+from kiara.module_config import KiaraModuleConfig
+from networkx import Graph
+from pydantic import Field
+
+KIARA_METADATA = {
+    "authors": [{"name": "Lena Jaskov", "email": "helena.jaskov@uni.lu"}],
+}
+
+# Here comes my attempt at building a find largest component module. Maybe need to add config for setting graph type as in CreateGraphFromEdgesTableModule
+
+
+class FindLargestComponentsModuleConfig(KiaraModuleConfig):
+
+    find_largest_component: bool = Field(
+        description="Find the largest component of a graph.", default=True
+    )
+
+    number_of_components: bool = Field(
+        description="Count the number of components.", default=True
+    )
+
+
+class FindLargestComponentsModule(KiaraModule):
+
+    _config_cls = FindLargestComponentsModuleConfig
+    _module_type_name = "find_largest_component"
+
+    def create_input_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        return {"graph": {"type": "network_graph", "doc": "The network graph."}}
+
+    def create_output_schema(
+        self,
+    ) -> typing.Mapping[
+        str, typing.Union[ValueSchema, typing.Mapping[str, typing.Any]]
+    ]:
+
+        result = {}
+        if self.get_config_value("find_largest_component"):
+            result["largest_component"] = {
+                "type": "network_graph",
+                "doc": "A sub-graph of the largest component of the graph.",
+            }
+
+        if self.get_config_value("number_of_components"):
+            result["number_of_components"] = {
+                "type": "integer",
+                "doc": "The number of components in the graph.",
+            }
+
+        return result
+
+    def process(self, inputs: ValueSet, outputs: ValueSet) -> None:
+
+        if self.get_config_value("find_largest_component"):
+            input_graph: Graph = inputs.get_value_data("graph")
+            undir_graph = nx.to_undirected(input_graph)
+            undir_components = nx.connected_components(undir_graph)
+            lg_component = max(undir_components, key=len)
+            subgraph = input_graph.subgraph(lg_component)
+
+            outputs.set_values(largest_component=subgraph)
+
+        if self.get_config_value("number_of_components"):
+            input_graph = inputs.get_value_data("graph")
+            undir_graph = nx.to_undirected(input_graph)
+            number_of_components = nx.number_connected_components(undir_graph)
+
+            outputs.set_values(number_of_components=number_of_components)
+
+
+# hmm.. how do I run this? kiara run playground.playground.find_largest_component
