620edbd6d65e574658207bab116e8a893d4f8825
MariellaCC <decrouy@gmail.com>
2021-07-21 18:47:09 +0200

    1st test w obs nb

---
+++ examples/streamlit/observable_test1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import streamlit as st
+from streamlit_observable import observable
+
+# st.markdown('<style>@import url("https://fonts.googleapis.com/css2?family=Palanquin:wght@400;700&display=swap")</style>', unsafe_allow_html=True)
+    #selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+
+timeSelected = st.sidebar.radio(label="Select time span", index=3, options=["day","week","month","year"])
+
+
+
+observers = observable("", 
+    notebook="d/50b89c7d50524163", 
+    targets=["viewof chart","style"], 
+    observe=["corpus_agg"],
+    redefine= {
+      "timeSelected": timeSelected
+    }
+)
+
+
+
+data = observers.get("corpus_agg")
+df = pd.DataFrame(data)
+st.dataframe(df)
