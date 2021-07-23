f60ab6aad0a8ca672fb4d42af7d9cd20e032fde5
Markus Binsteiner <markus@frkl.io>
2021-07-23 09:12:11 +0200

    examples

---
+++ data/README.md
@@ -0,0 +1,3 @@
+In general, data should not be added to the `kiara_modules.playground` repo, since that would blow up the size too much, given that several people use it to do their prototyping and testing.
+
+But you can add data into this directory for convenience. It is added to `.gitignore`, so git will in general ignore it.
--- examples/streamlit/streamlit.py
+++ examples/streamlit/streamlit.py
@@ -5,24 +5,39 @@
 # streamlit run examples/streamlit/streamlit.py
 
 
-import pyarrow as pa
 import streamlit as st
 from kiara import Kiara
+from kiara.workflow.kiara_workflow import KiaraWorkflow
 
-kiara = Kiara()
+st.title("Kiara/observable proof-of-concept app")
 
-st.title("My first kiara streamlit app")
+if "kiara" not in st.session_state:
+    print("CREATE KIARA")
+    kiara = Kiara()
+    st.session_state["kiara"] = kiara
+else:
+    kiara = st.session_state["kiara"]
+
+if "workflow" not in st.session_state:
+    print("CREATE WORKFLOW")
+    workflow: KiaraWorkflow = kiara.create_workflow("table.import.from_local_file")
+    st.session_state["workflow"] = workflow
+else:
+    workflow = st.session_state["workflow"]
 
 
-result_table: pa.Table = kiara.run(
-    "table.import.from_local_file",
-    inputs={
-        "path": "/home/markus/projects/dharpa/notebooks/NetworkXAnalysis/JournalEdges1902.csv"
-    },
-    output_name="table",
-    resolve_result=True,
-)
+with st.form(key="Workflow inputs"):
+    filename = st.text_input("Enter a file path:")
+    submit_button = st.form_submit_button(label="Submit")
 
-df = result_table.to_pandas()
 
-st.write(df)
+workflow.inputs.set_value("path", filename)
+
+state = workflow.get_current_state()
+# st.write(state.dict())
+
+output = workflow.outputs.get_all_value_objects()
+st.write(output)
+
+data = workflow.outputs.get_value_data("table")
+st.write(data.to_pandas())
