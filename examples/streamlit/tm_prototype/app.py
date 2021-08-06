fc1b49efe7530103770c035244828a9f5f072118
MariellaCC <decrouy@gmail.com>
2021-08-06 16:56:36 +0200

    kiara onboarding

--- examples/streamlit/tm_prototype/app.py
+++ examples/streamlit/tm_prototype/app.py
@@ -1,25 +1,22 @@
 import os
 import streamlit as st
-import numpy as np
+from kiara import Kiara
 
 # Custom imports 
 from multipage import MultiPage
-from pages import data_upload, timestamped_corpus
+from pages import files_onboarding, timestamped_corpus
 
 app = MultiPage()
 
 # Title of the main page
 st.title("TM Streamlit test")
-#col1, col2 = st.beta_columns(2)
-#col1.image(display, width = 400)
-#col2.title("TM Streamlit test")
+
+kiara = Kiara.instance()
+st.session_state["kiara"] = kiara
 
 # Add all your application here
-app.add_page("Upload Data", data_upload.app)
+app.add_page("Upload Data", files_onboarding.app)
 app.add_page("Timestamped data", timestamped_corpus.app)
-#app.add_page("Machine Learning", machine_learning.app)
-#app.add_page("Data Analysis",data_visualize.app)
-#app.add_page("Y-Parameter Optimization",redundant.app)
 
 # The main app
 app.run()
---
+++ examples/streamlit/tm_prototype/pages/files_onboarding.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from kiara import Kiara
+from kiara_modules.playground.markus.streamlit import onboard_file_bundle
+
+
+def app():
+
+    kiara = st.session_state["kiara"]
+
+    st.markdown("## Create dataframe")
+    st.markdown("Select sample data from one publication folder located in  kiara_modules.playground/examples/newspaper_corpora/CI_newspaper_subcorpora") 
+    st.markdown("Once you're ready, go to the next step via select box on the left")
+
+    uploaded_files = st.file_uploader("Add files", type = 'txt', accept_multiple_files=True)
+    
+    alias = 'tm1'
+
+    file_names = [i.name for i in uploaded_files]
+    file_txt = [i.getvalue().decode("utf-8") for i in uploaded_files]
+
+    df_onboard = pd.DataFrame({
+                'File name': file_names,
+                'File content': file_txt
+            })
+
+    
+    st.markdown("### Onboarded file preview")
+    st.markdown("Once you're ready selecting files, click 'onboard' below")
+    
+    st.dataframe(df_onboard)
+    onboard_button = st.button(label="Onboard")
+    
+    if onboard_button:
+        onboard_file_bundle(kiara=kiara, uploaded_files=uploaded_files, aliases=alias)
+
+    if kiara.data_store.load_value(alias):
+
+        table_value = kiara.data_store.load_value(alias)
+        table: pa.Table = table_value.get_value_data()
+        df = table.to_pandas()
+        
+        st.session_state.data = df
