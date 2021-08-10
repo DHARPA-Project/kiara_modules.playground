1c984a416ebb6a4227102186216ac77f950cf0ec
MariellaCC <decrouy@gmail.com>
2021-08-10 19:26:01 +0200

    viz options

--- examples/streamlit/tm_prototype/pages/timestamped_corpus.py
+++ examples/streamlit/tm_prototype/pages/timestamped_corpus.py
@@ -16,7 +16,12 @@ def app():
     sql_query_month = "SELECT strptime(concat('01/', month, '/', year), '%d/%m/%Y') as date, pub_name, count FROM (SELECT YEAR(date) as year, MONTH(date) as month, pub_name, count(*) as count FROM data group by YEAR(date), MONTH(date), pub_name ORDER BY year, month, pub_name) AS agg"
     sql_query_year = "SELECT strptime(concat('01/01/', year), '%d/%m/%Y') as date, pub_name, count FROM (SELECT YEAR(date) as year, pub_name, count(*) as count FROM data group by YEAR(date), pub_name ORDER BY year, pub_name) AS agg"
 
-    unit = "year"
+
+    unit = st.selectbox("Aggregate by", ('year', 'month', 'day'))
+
+    scaleType = st.selectbox("Scale by", ('color', 'height'))
+
+    axisLabel = st.selectbox("Axis", ('5-year', 'year', 'month', 'day'))
     
     #unit = st.radio("Display data by", ("year", "month", "day"))
 
@@ -34,20 +39,20 @@ def app():
     query_result_table = query_result_value.get_value_data()
     
 
-
     data = list(query_result_table.to_pandas().to_dict(orient="index").values())
     data_json = json.dumps(data, default=str)
     cleaned_data = json.loads(data_json)
+
+    
+
 
 
     observers = observable(
         "Test",
         notebook="d/d1e17c291019759e",
         targets=["viewof chart", "style"],
-        redefine={"timeSelected": unit, "data": cleaned_data},
+        redefine={"timeSelected": unit, "data": cleaned_data, "scaleType": scaleType, "axisLabel": axisLabel},
     )
-
-    # , "scaleType": scaleType, "axisLabel": axisLabel
 
     st.dataframe(query_result_table.to_pandas())
 
