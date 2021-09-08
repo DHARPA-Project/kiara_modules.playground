b97d4dc41c313e02387b2cc3c6a018630fe7c7b2
MariellaCC <decrouy@gmail.com>
2021-09-08 21:27:36 +0200

    filename metadata wip

--- examples/streamlit/tm_prototype/pages/get_file_metadata.py
+++ examples/streamlit/tm_prototype/pages/get_file_metadata.py
@@ -12,38 +12,37 @@ def app():
     st.markdown('Wait for file preview to be displayed, before proceeding to the next step')
     st.markdown('*Temporary screen for file names metadata step*')
     st.markdown('*This module will be completed at a later stage *')
+
+    process_metadata = st.radio("Do your file names contain metadata?",("no", "yes"))
     
-    my_expander = st.sidebar.expander(label='Settings')
-    
-    with my_expander:
-    
-        process_metadata = st.radio("Do your file names contain metadata?",("yes", "no"))
+    st.write("Supported pattern: '/sn86069873/1900-01-05/'")
+    st.write("LCCN title information and publication date (yyyy-mm-dd)")
 
-        if process_metadata == "yes":
+    if process_metadata:
+        if process_metadata == 'no':
+            st.session_state.metada = False
+        
+        elif process_metadata == 'yes':
+            # load the pipeline file and create a workflow
+            augment_pipeline = os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                "..",
+                "..",
+                "newspaper_corpora",
+                "augment_newspaper_corpora_table.json",
+            )
+        
+            workflow = kiara.create_workflow(augment_pipeline)
+            # set our table as input to the workflow
+            workflow.inputs.set_value("value_id", table_value.id)
 
-            file_format = st.radio("Which pattern describes your file names?", ('/sn86069873/1900-01-05/', ''))
+            # retrieve the actual table value
+            augmented_table_value = workflow.outputs.get_value_obj("table")
 
-    if file_format == '/sn86069873/1900-01-05/':
-        # load the pipeline file and create a workflow
-        augment_pipeline = os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "..",
-            "..",
-            "newspaper_corpora",
-            "augment_newspaper_corpora_table.json",
-        )
-    
-        workflow = kiara.create_workflow(augment_pipeline)
-        # set our table as input to the workflow
-        workflow.inputs.set_value("value_id", table_value.id)
+            st.session_state.augmented_data = augmented_table_value
 
-        # retrieve the actual table value
-        augmented_table_value = workflow.outputs.get_value_obj("table")
-
-        st.session_state.augmented_data = augmented_table_value
-
-        table = augmented_table_value.get_value_data()
-        df = table.to_pandas()
-        st.write('Result preview')
-        AgGrid(df.head(50))
+            table = augmented_table_value.get_value_data()
+            df = table.to_pandas()
+            st.write('Result preview')
+            AgGrid(df.head(50))
